bcd585d90eb62f363ac2a1c5b6e9cbfbf99faa5e
350016z <100845242+350016z@users.noreply.github.com>
2024-03-26 09:19:35 +0800

    FEAT:integrated data

--- traffic_volume/Integrated_forecastdata.py
+++ traffic_volume/Integrated_forecastdata.py
@@ -26,19 +26,19 @@ def download_and_extract_tar(url, extract_path):
     else:
         print("下載失敗！")
 
-def traffic_volume(new_df ,file_path):
+def get_trafficvolume(new_df ,file_path):
     #header=None，這樣Pandas將不會將第一列視為標籤，而是將其視為數據行
     data = pd.read_csv(file_path,header=None)
     data.columns = ['TimeInterval', 'RoadSection', 'Direction', 'VehicleType', 'TrafficVolume']
-    #new_df = pd.DataFrame(columns=['TimeInterval', 'RoadSection', 'Direction', 'PCU', 'Week', 'Holiday'])
+
+    # 使用映射替換GantryID的值
+    data.replace({'RoadSection':mapping['RoadSection']}, inplace=True)
     
     # 根據 GantryID 分組並分類 VehicleType(非小車*1.4)
-    for gantry_id, group_data in data.groupby('RoadSection'):
+    for (road_section, direction), group_data in data.groupby(['RoadSection', 'Direction']):
         if group_data['RoadSection'].values[0][:3] == '03A': 
             continue
         else:
-            #print(gantry_id)
-            #print(group_data)
             large_car, small_car = 0, 0
             for vt, tv in zip(group_data['VehicleType'], group_data['TrafficVolume']):
                 if vt in [41, 42, 5]:
@@ -50,11 +50,25 @@ def traffic_volume(new_df ,file_path):
             TimeInterval = data.loc[last_row_index, 'TimeInterval'].values[0]  # 使用.values[0]取出值
             RoadSection = data.loc[last_row_index, 'RoadSection'].values[0] 
             Direction = data.loc[last_row_index, 'Direction'].values[0] 
-            new_df = pd.concat([new_df, pd.DataFrame({'TimeInterval': [TimeInterval], 'RoadSection':[RoadSection], 'Direction': [Direction], 'PCU': [pcu]})], ignore_index=True)
-    return new_df
-    # 重設索引以解決問題
-   
+            new_df = pd.concat([new_df, pd.DataFrame({'TimeInterval': [TimeInterval], 'RoadSection':[RoadSection], 'Direction': [Direction], 'PCU': [pcu]})])
 
+    return new_df
+
+def count_trafficvolume(file_path, original_order):
+    data = pd.read_csv(file_path)
+    count_file = pd.DataFrame(columns=['RoadSection','Direction','PCU'])
+
+    for (road_section, direction), group_data in data.groupby(['RoadSection', 'Direction']):
+        count_pcu = group_data['PCU'].sum()
+        RoadSection = group_data['RoadSection'].values[0] 
+        Direction = group_data['Direction'].values[0]
+        count_file = pd.concat([count_file, pd.DataFrame({'RoadSection': [RoadSection],'Direction':[Direction],'PCU':[count_pcu]})])
+    
+    count_file = count_file.sort_values(by=['RoadSection'], key=lambda x: x.map({v: i for i, v in enumerate(original_order)}))
+    count_file.to_csv('count_file.csv', index=False, encoding='utf-8-sig')
+    print("數據已保存到 'count_file.csv'")
+
+    
 
 
 def delete_file(file_name):
@@ -67,6 +81,8 @@ def delete_file(file_name):
         print(f"權限錯誤。無法刪除目錄及其內容: {file_name}")
     except Exception as e:
         print(f"發生錯誤: {e}")
+
+
 
 
 
@@ -93,17 +109,26 @@ extract_path = "./extracted_files"
 # 函式下載並解壓縮.tar檔案
 download_and_extract_tar(tar_url, extract_path)
 
-# 取得data資訊
+# 取得m03a_data資訊
+# 加載attribute_mapping文件
+with open('traffic_volume_attributes.json', 'r', encoding='utf-8') as file:
+    mapping = json.load(file)
 new_df = pd.DataFrame(columns=['TimeInterval', 'RoadSection', 'Direction', 'PCU'])
 matching_files = find_matching_files(root_dir, prefix, date)
-# 輸出找到的檔案路徑
 for file_path in matching_files:
-    new_df = traffic_volume(new_df, file_path)
+    new_df = get_trafficvolume(new_df, file_path)
     print(file_path)
+new_df.to_csv('trafficvolume_20230101.csv', index=False, encoding='utf-8-sig')
+print("數據已保存到 'trafficvolume_20230101.csv'")
 
-new_df.reset_index(drop=True, inplace=True)
-new_df.to_csv('test.csv', index=False, encoding='utf-8-sig')
-print("數據已保存到 'test.csv'")
+# 計算各路段交通量
+data = pd.read_csv(file_path)
+data.columns = ['TimeInterval', 'RoadSection', 'Direction', 'VehicleType', 'TrafficVolume']
+data.replace({'RoadSection':mapping['RoadSection']}, inplace=True)
+original_order = data['RoadSection'].unique().tolist()
+countfilepath = "trafficvolume_20230101.csv"
+count_trafficvolume(countfilepath, original_order)
+
 
 # 刪除文件
 delete_file('extracted_files')
