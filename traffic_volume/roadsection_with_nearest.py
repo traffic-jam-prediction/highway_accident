9bd3e32f67107cdfe2cd86a96551385ae3500d14
350016z <100845242+350016z@users.noreply.github.com>
2024-04-04 02:03:08 +0800

    Modify: swap from and to

--- traffic_volume/roadsection_with_nearest.py
+++ traffic_volume/roadsection_with_nearest.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import json
 
-csv_file = 'C:\\Users\\julia\\Desktop\\交通局\\TDCS_M03A_20240227_153500.csv'
-
 def get_highway_midpoint():
     highway1 = pd.read_csv('../highway_information/國道1號.csv', encoding='utf-8')
     highway1_h = pd.read_csv('../highway_information/國道1號_高架.csv', encoding='utf-8')
@@ -24,15 +22,15 @@ def get_highway_midpoint():
                                     'highway': ['國道1號','國道1號'],
                                     'direction':['N','S'],
                                     'midpoint': [midpoint,midpoint], 
-                                    'from_mil': [from_mil,from_mil], 
-                                    'to_mil': [to_mil,to_mil]})
+                                    'from_mil': [to_mil,from_mil], 
+                                    'to_mil': [from_mil,to_mil]})
             else:
                 new_df = pd.concat([new_df, pd.DataFrame({'roadsection': [roadsection, roadsection], 
                                                        'highway': ['國道1號','國道1號'], 
                                                        'direction':['N','S'],
                                                        'midpoint': [midpoint,midpoint], 
-                                                       'from_mil': [from_mil,from_mil],  
-                                                       'to_mil': [to_mil,to_mil]})], ignore_index=True)
+                                                       'from_mil': [to_mil,from_mil],  
+                                                       'to_mil': [from_mil,to_mil]})], ignore_index=True)
     # 合併國道一號高架的資料
     for index, row in highway1_h.iterrows():
         if index < len(highway1_h) - 1:
@@ -44,8 +42,8 @@ def get_highway_midpoint():
                                                    'highway': ['國道1號_高架','國道1號_高架'], 
                                                    'direction':['N','S'],
                                                    'midpoint': [midpoint,midpoint], 
-                                                   'from_mil': [from_mil,from_mil], 
-                                                   'to_mil': [to_mil,to_mil]})], ignore_index=True)
+                                                   'from_mil': [to_mil,from_mil], 
+                                                   'to_mil': [from_mil,to_mil]})], ignore_index=True)
     # 合併國道三號的資料
     for index, row in highway3.iterrows():
         if index < len(highway3) - 1:
@@ -57,8 +55,8 @@ def get_highway_midpoint():
                                                    'highway': ['國道3號','國道3號'], 
                                                    'direction':['N','S'],
                                                    'midpoint': [midpoint,midpoint], 
-                                                   'from_mil': [from_mil,from_mil], 
-                                                   'to_mil': [to_mil,to_mil]})], ignore_index=True)
+                                                   'from_mil': [to_mil,from_mil], 
+                                                   'to_mil': [from_mil,to_mil]})], ignore_index=True)
     # 合併國道五號的資料
     for index, row in highway5.iterrows():
         if index < len(highway5) - 1:
@@ -70,8 +68,8 @@ def get_highway_midpoint():
                                                    'highway': ['國道5號','國道5號'], 
                                                    'direction':['N','S'],
                                                    'midpoint': [midpoint,midpoint], 
-                                                   'from_mil': [from_mil,from_mil], 
-                                                   'to_mil': [to_mil,to_mil]})], ignore_index=True)
+                                                   'from_mil': [to_mil,from_mil], 
+                                                   'to_mil': [from_mil,to_mil]})], ignore_index=True)
     #new_df.to_csv('../highway_information/roadsection_TEST.csv', encoding='utf-8-sig', index=False)
     #print("roadsection.csv已儲存")
     return(new_df)
@@ -86,14 +84,14 @@ def get_gantrid():
     return gantryID
 
 
-def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
-    df = roadsection_and_midpoint
+def find_nearest_detection_points():
+    df = get_highway_midpoint()
     df['nearest_gantry1'] = None
     df['nearest_gantry2'] = None
     for index, row in df.iterrows():
         if row['highway'] == '國道1號' and row['direction'] == 'S':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '01F'and gid.endswith("S"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -104,7 +102,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]  
         if row['highway'] == '國道1號' and row['direction'] == 'N':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '01F'and gid.endswith("N"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -115,7 +113,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]  
         if row['highway'] == '國道1號_高架' and row['direction'] == 'S':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '01H'and gid.endswith("S"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -126,7 +124,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]  
         if row['highway'] == '國道1號_高架' and row['direction'] == 'N':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '01H'and gid.endswith("N"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -137,7 +135,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]  
         if row['highway'] == '國道3號' and row['direction'] == 'S':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '03F'and gid.endswith("S"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -148,7 +146,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]
         if row['highway'] == '國道3號' and row['direction'] == 'N':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '03F'and gid.endswith("N"):  
                     distance = abs(row['midpoint'] - int(gid[3:7])/10)
                     distances.append((gid, distance))
@@ -159,7 +157,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]    
         if row['highway'] == '國道5號' and row['direction'] == 'S':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '05F'and gid.endswith("S"):  
                     distance = abs(row['midpoint'] - int(gid[4:7])/10)
                     distances.append((gid, distance))
@@ -170,7 +168,7 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]
         if row['highway'] == '國道5號' and row['direction'] == 'N':
             distances = []
-            for gid in gantryID:
+            for gid in get_gantrid():
                 if gid[:3] == '05F'and gid.endswith("N"):  
                     distance = abs(row['midpoint'] - int(gid[4:7])/10)
                     distances.append((gid, distance))
@@ -180,11 +178,10 @@ def find_nearest_detection_points(roadsection_and_midpoint, gantryID):
                 df.at[index, 'nearest_gantry1'] = nearest_two[0][0]  
                 df.at[index, 'nearest_gantry2'] = nearest_two[1][0]    
 
-    df.to_csv('../highway_information/roadsectiondata_with_nearest.csv', encoding='utf-8-sig', index=False)
-    print("roadsection_with_nearest.csv已儲存")
+    #df.to_csv('../highway_information/roadsectiondata_with_nearest.csv', encoding='utf-8-sig', index=False)
+    #print("roadsection_with_nearest.csv已儲存")
+    return(df)
 
 
 if __name__ == '__main__':
-    roadsection_and_midpoint = get_highway_midpoint()
-    gantryID = get_gantrid()
-    nearest_two_points = find_nearest_detection_points(roadsection_and_midpoint, gantryID)
+    roadsection_with_nearest = find_nearest_detection_points()
