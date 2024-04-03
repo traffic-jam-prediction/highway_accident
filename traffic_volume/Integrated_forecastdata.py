329e66c6589e59e60f02d0cd9bca803c5cca9c41
350016z <100845242+350016z@users.noreply.github.com>
2024-04-04 02:05:00 +0800

    Feat:integrate speed and traffic volume of m03a&m05a files

--- traffic_volume/Integrated_forecastdata.py
+++ traffic_volume/Integrated_forecastdata.py
@@ -4,6 +4,9 @@ import json
 import tarfile
 import os
 import shutil
+import numpy as np
+from datetime import datetime, timedelta
+from roadsection_with_nearest import find_nearest_detection_points
 
 
 def download_and_extract_tar(url, extract_path):
@@ -24,48 +27,114 @@ def download_and_extract_tar(url, extract_path):
     else:
         print("下載失敗！")
 
-def get_trafficvolume(new_df ,file_path):
+
+def get_trafficvolume(m03a_file, m05a_file):
     #header=None，這樣Pandas將不會將第一列視為標籤，而是將其視為數據行
-    data = pd.read_csv(file_path,header=None)
-    data.columns = ['TimeInterval', 'RoadSection', 'Direction', 'VehicleType', 'TrafficVolume']
-    data.replace({'RoadSection':mapping['RoadSection']}, inplace=True)
+    df = find_nearest_detection_points()
+    m03a = pd.read_csv(m03a_file,header=None)
+    m03a.columns = ['TimeInterval', 'GantryID', 'Direction', 'VehicleType', 'TrafficVolume']
+    m05a = pd.read_csv(m05a_file,header=None)
+    m05a.columns = ['TimeInterval','GantryFrom', 'GantryTo', 'VehicleType', 'SpaceMeanSpeed', 'Traffic']
+    # 將 data 資料框轉換為字典，每一行轉換為一個字典(為了效率)
+    m03a_dict = m03a.to_dict('records')
+    m05a_dict = m05a.to_dict('records')
+    df_dict = df.to_dict('records')
+    new_df = pd.DataFrame(columns=['Date','TimeInterval','RoadSection','Highway','Direction',
+                               'M03A_PCU','M03A_PCU_BigGap','M05A_SpaceMeanSpeed','M05A_PCU'])
+
+    for row in df_dict:
+        pcu1, pcu2, speed, pcu_m05a, pcua, pcub = 0, 0, 0, 0, 0, 0
+        for m03a_data in m03a_dict:
+            if m03a_data['GantryID'] == row['nearest_gantry1']:
+                if m03a_data['VehicleType'] in [41, 42, 5]:
+                    pcu1 += m03a_data['TrafficVolume'] * 1.4
+                else:
+                    pcu1 += m03a_data['TrafficVolume']
+            elif m03a_data['GantryID'] == row['nearest_gantry2']:
+                if m03a_data['VehicleType'] in [41, 42, 5]:
+                    pcu2 += m03a_data['TrafficVolume'] * 1.4
+                else:
+                    pcu2 += m03a_data['TrafficVolume']
+        if pcu1==0 or pcu2==0:
+            pcu=pcu1+pcu2
+        else:
+            pcu = (pcu1 + pcu2) / 2
+        if abs(pcu1-pcu2) > 50:
+            biggap = 1
+        else:
+            biggap = np.nan
     
-    # 根據 GantryID 分組並分類 VehicleType(非小車*1.4)
-    for (road_section, direction), group_data in data.groupby(['RoadSection', 'Direction']):
-        if group_data['RoadSection'].values[0][:3] == '03A': 
+        for m05a_data in m05a_dict:
+            if row['direction'] == 'S':
+                if (m05a_data['GantryFrom'][:3] == row['nearest_gantry1'][:3] and m05a_data['GantryFrom'][:3] == '05F'and m05a_data['GantryFrom'].endswith("S") and 
+                    int(m05a_data['GantryFrom'][4:7])/10 <= row['midpoint'] < int(m05a_data['GantryTo'][4:7])/10) :
+                    speed += (m05a_data['SpaceMeanSpeed'] * m05a_data['Traffic'])
+                    if m05a_data['VehicleType'] in [41, 42, 5]:
+                        pcua += m05a_data['Traffic'] 
+                    else:
+                        pcub += m05a_data['Traffic']
+                elif(m05a_data['GantryFrom'][:3] == row['nearest_gantry1'][:3] and m05a_data['GantryFrom'][:3] != '05F' and m05a_data['GantryFrom'].endswith('S') and 
+                    int(m05a_data['GantryFrom'][3:7])/10 <= row['midpoint'] < int(m05a_data['GantryTo'][3:7])/10) :
+                    speed += (m05a_data['SpaceMeanSpeed'] * m05a_data['Traffic'])
+                    if m05a_data['VehicleType'] in [41, 42, 5]:
+                        pcua += m05a_data['Traffic'] 
+                    else:
+                        pcub += m05a_data['Traffic']
+            elif row['direction'] == 'N': 
+                if (m05a_data['GantryFrom'][:3] == row['nearest_gantry1'][:3] and m05a_data['GantryFrom'][:3] == '05F' and m05a_data['GantryFrom'].endswith("N") and 
+                    int(m05a_data['GantryTo'][4:7])/10 <= row['midpoint'] < int(m05a_data['GantryFrom'][4:7])/10) :
+                    speed += (m05a_data['SpaceMeanSpeed'] * m05a_data['Traffic'])
+                    if m05a_data['VehicleType'] in [41, 42, 5]:
+                        pcua += m05a_data['Traffic'] 
+                    else:
+                        pcub += m05a_data['Traffic']
+                elif (m05a_data['GantryFrom'][:3] == row['nearest_gantry1'][:3] and m05a_data['GantryFrom'][:3] != '05F' and m05a_data['GantryFrom'].endswith("N") and 
+                    int(m05a_data['GantryTo'][3:7])/10 <= row['midpoint'] < int(m05a_data['GantryFrom'][3:7])/10) :
+                    speed += (m05a_data['SpaceMeanSpeed'] * m05a_data['Traffic'])
+                    if m05a_data['VehicleType'] in [41, 42, 5]:
+                        pcua += m05a_data['Traffic'] 
+                    else:
+                        pcub += m05a_data['Traffic']
+        pcu_m05a = pcua * 1.4 + pcub
+        if pcua + pcub ==0:
             continue
         else:
-            large_car, small_car = 0, 0
-            for vt, tv in zip(group_data['VehicleType'], group_data['TrafficVolume']):
-                if vt in [41, 42, 5]:
-                    large_car += tv
-                else:
-                    small_car += tv
-            pcu = round(large_car * 1.4 + small_car, 2)
-            last_row_index = group_data.tail(1).index
-            TimeInterval = data.loc[last_row_index, 'TimeInterval'].values[0]  # 使用.values[0]取出值
-            RoadSection = data.loc[last_row_index, 'RoadSection'].values[0] 
-            Direction = data.loc[last_row_index, 'Direction'].values[0] 
-            new_df = pd.concat([new_df, pd.DataFrame({'TimeInterval': [TimeInterval], 'RoadSection':[RoadSection], 'Direction': [Direction], 'PCU': [pcu]})])
-
+            speed = round(speed/( pcua + pcub ),1)
+        date, time_interval = m03a_data['TimeInterval'].split(' ')
+        if new_df.empty:
+            new_df = pd.DataFrame({'Date':[date],'TimeInterval': [time_interval],'RoadSection':row['roadsection'],
+                                   'Highway':row['highway'],'Direction':row['direction'],'M03A_PCU':[pcu],
+                               'M03A_PCU_BigGap': [biggap],'M05A_SpaceMeanSpeed': [speed],'M05A_PCU':[pcu_m05a]})
+        else:
+            new_df = pd.concat([new_df, pd.DataFrame({'Date': [date], 
+                                            'TimeInterval': [time_interval], 
+                                            'RoadSection':row['roadsection'],
+                                            'Highway':row['highway'],
+                                            'Direction':row['direction'],
+                                            'M03A_PCU':[pcu],
+                                            'M03A_PCU_BigGap': [biggap],
+                                            'M05A_SpaceMeanSpeed': [speed],
+                                            'M05A_PCU':[pcu_m05a]
+                                            })], ignore_index=True)
+        
     return new_df
 
-def count_trafficvolume(file_path, original_order):
-    data = pd.read_csv(file_path)
-    count_file = pd.DataFrame(columns=['RoadSection','Direction','PCU'])
-
-    for (road_section, direction), group_data in data.groupby(['RoadSection', 'Direction']):
-        count_pcu = group_data['PCU'].sum()
+def count_trafficvolume(countfile_path):
+    # 為了保留原始資料路段的排序順序(才能呈現由北到南的順序)
+    data1 = find_nearest_detection_points()
+    original_order = data1['roadsection'].unique().tolist()
+    
+    count_file = pd.DataFrame(columns=['RoadSection','Direction','M03A_PCU'])
+    data2 = pd.read_csv(countfile_path)
+    for (road_section, direction), group_data in data2.groupby(['RoadSection', 'Direction']):
+        count_pcu = group_data['M03A_PCU'].sum()
         RoadSection = group_data['RoadSection'].values[0] 
         Direction = group_data['Direction'].values[0]
-        count_file = pd.concat([count_file, pd.DataFrame({'RoadSection': [RoadSection],'Direction':[Direction],'PCU':[count_pcu]})])
+        count_file = pd.concat([count_file, pd.DataFrame({'RoadSection': [RoadSection],'Direction':[Direction],'M03A_PCU':[count_pcu]})])
     
     count_file = count_file.sort_values(by=['RoadSection'], key=lambda x: x.map({v: i for i, v in enumerate(original_order)}))
     count_file.to_csv('count_file.csv', index=False, encoding='utf-8-sig')
     print("數據已保存到 'count_file.csv'")
-
-    
-
 
 def delete_file(file_name):
     try:
@@ -78,6 +147,9 @@ def delete_file(file_name):
     except Exception as e:
         print(f"發生錯誤: {e}")
 
+def daterange(start_date, end_date):
+    for n in range(int((end_date - start_date).days) + 1):
+        yield start_date + timedelta(n)
 
 def find_matching_files(root_dir, prefix, date):
     matching_files = []
@@ -88,39 +160,52 @@ def find_matching_files(root_dir, prefix, date):
     return matching_files
 
 if __name__ == '__main__':
-    root_dir = r'.\extracted_files\M03A\20230101'
-    prefix = 'M03A'
-    date = '20230101'
-
-    # 網站.tar檔案的URL
-    traffic_url = "https://tisvcloud.freeway.gov.tw/history/TDCS/M03A/"
-    tar_day = "M03A_20230101.tar.gz"
-    tar_url = os.path.join(traffic_url, tar_day)
-
-    # 解壓縮的路徑
-    extract_path = "./extracted_files"
-    # 函式下載並解壓縮.tar檔案
-    download_and_extract_tar(tar_url, extract_path)
-
-    # 取得m03a_data資訊
-    # 加載attribute_mapping文件
     with open('traffic_volume_attributes.json', 'r', encoding='utf-8') as file:
         mapping = json.load(file)
-    new_df = pd.DataFrame(columns=['TimeInterval', 'RoadSection', 'Direction', 'PCU'])
-    matching_files = find_matching_files(root_dir, prefix, date)
-    for file_path in matching_files:
-        new_df = get_trafficvolume(new_df, file_path)
-        print(file_path)
-    new_df.to_csv('trafficvolume_20230101.csv', index=False, encoding='utf-8-sig')
-    print("數據已保存到 'trafficvolume_20230101.csv'")
+
+    start_date_str = input("請輸入開始日期（格式如20230101）：")
+    end_date_str = input("請輸入結束日期（格式如20231031）：")
+    start_date = datetime.strptime(start_date_str, "%Y%m%d")
+    end_date = datetime.strptime(end_date_str, "%Y%m%d")
+    matching_files_m03a = []
+    matching_files_m05a = []
+
+    for single_date in daterange(start_date, end_date):
+        date = single_date.strftime("%Y%m%d")
+        root_dir_m03a = f'./extracted_files/M03A/{date}'
+        prefix_m03a = 'M03A'
+        root_dir_m05a = f'./extracted_files/M05A/{date}'
+        prefix_m05a = 'M05A'
+
+        # 網站.tar檔案的URL組合方式
+        m03a_url = "https://tisvcloud.freeway.gov.tw/history/TDCS/M03A/"
+        tar_day_m03a = f"M03A_{date}.tar.gz"
+        tar_url_m03a = os.path.join(m03a_url, tar_day_m03a)
+        m05a_url = "https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/"
+        tar_day_m05a = f"M05A_{date}.tar.gz"
+        tar_url_m05a = os.path.join(m05a_url, tar_day_m05a)
+
+        # 解壓縮的路徑
+        extract_path = "./extracted_files"
+        # 函式下載並解壓縮.tar檔案
+        download_and_extract_tar(tar_url_m03a, extract_path)
+        download_and_extract_tar(tar_url_m05a, extract_path)
+        # 取得root_dir目錄下，所有m03a檔案的路徑   
+        matching_files_m03a.extend(find_matching_files(root_dir_m03a, prefix_m03a, date))
+        matching_files_m05a.extend(find_matching_files(root_dir_m05a, prefix_m05a, date))
+
+    # 取得m03a_data資訊
+    new_df = pd.DataFrame(columns=['Date','TimeInterval','RoadSection','Highway','Direction',
+                               'M03A_PCU','M03A_PCU_BigGap','M05A_SpaceMeanSpeed','M05A_PCU'])
+    for file_m03a, file_m05a in zip(matching_files_m03a, matching_files_m05a):
+        new_df = pd.concat([new_df, get_trafficvolume(file_m03a, file_m05a)], ignore_index=True)
+        print(file_m03a, file_m05a)
+    new_df.to_csv('trafficvolume_0101.csv', index=False, encoding='utf-8-sig')
+    print("數據已保存到 'trafficvolume.csv'")
 
     # 計算各路段交通量
-    data = pd.read_csv(file_path)
-    data.columns = ['TimeInterval', 'RoadSection', 'Direction', 'VehicleType', 'TrafficVolume']
-    data.replace({'RoadSection':mapping['RoadSection']}, inplace=True)
-    original_order = data['RoadSection'].unique().tolist()
-    countfilepath = "trafficvolume_20230101.csv"
-    count_trafficvolume(countfilepath, original_order)
+    countfile_path = 'trafficvolume_0101.csv'
+    count_trafficvolume(countfile_path)
 
 
     # 刪除文件
