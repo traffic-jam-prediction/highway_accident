fc4f6753489231df8c512b51d0ff2b8eb131ff1f
350016z <100845242+350016z@users.noreply.github.com>
2024-05-01 00:17:00 +0800

    Feat : add highway colume

--- accident_road/mapping_highwaymil.py
+++ accident_road/mapping_highwaymil.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import datetime
 
 # 定義函式來判斷事件位置
 def detect_location(kilometer, highway):
@@ -18,10 +18,11 @@ df = pd.read_csv("A3_accident.csv", encoding='utf-8')
 new_df = pd.DataFrame(columns=['時間','事故類別', '路段', '方向'])
 
 for index, row in df.iterrows():
-    if not pd.isnull(row['公里']):  # 檢查 '公里' 欄位是否為 NaN
-        row['公里'] = int(row['公里']) # 確保公里數為整數
-        highway, ai, kilometer, direction = row['路線'], row['事故類別'], row['公里'], '北上' if '北側' in row['向'] else '南下'
-        if row[' 道路型態'] == "高架道路":
+
+    if not pd.isnull(row['公里']) :  
+        row['公里'] = int(row['公里']) 
+        highway, ai, kilometer, direction = row['路線'], row['事故類別'], row['公里'], 'N' if '北側' in row['向'] else 'S'
+        if row[' 道路型態'] == "高架道路" and highway in "國道1號":
             if kilometer>71:
                 continue    # 跳過後續程式碼，處理下一行資料
             if highway in ["國道1號"]:
@@ -30,14 +31,22 @@ for index, row in df.iterrows():
                 else:
                     highway = highway+"_高架"
                     location = detect_location(kilometer, highway) 
-                location = location + "(高架)"
-                time_info = f"{int(row['年'])}-{int(row['月'])}-{int(row['日'])} {int(row['時'])}:{int(row['分'])}:{int(row['秒'])}"
-                new_df = pd.concat([new_df, pd.DataFrame({'時間': [time_info], '事故類別':[ai], '路段': [location], '方向': [direction]})], ignore_index=True)
-        else :
-            if highway in ["國道1號","國道3號","國道5號"]:
-                location = detect_location(kilometer, highway) 
-                time_info = f"{int(row['年'])}-{int(row['月'])}-{int(row['日'])} {int(row['時'])}:{int(row['分'])}:{int(row['秒'])}"
-                new_df = pd.concat([new_df, pd.DataFrame({'時間': [time_info], '事故類別':[ai], '路段': [location], '方向': [direction]})], ignore_index=True)
+                
+        elif highway in ["國道1號","國道3號","國道5號"]:
+            location = detect_location(kilometer, highway) 
+        else:
+            continue
+        
+        date = datetime.strptime(f"{int(row['年'])}-{int(row['月'])}-{int(row['日'])}", '%Y-%m-%d')
+        time = datetime.strptime(f"{int(row['時'])}:{int(row['分'])}:{int(row['秒'])}", '%H:%M:%S')
+        minute = 5 * (time.minute // 5)
+        time = f"{time.hour:02d}:{minute:02d}"
+
+        if new_df.empty:     
+            new_df = pd.DataFrame({'Date': [date], 'Time':[time],'Class':[ai], 'RoadSection': [location], 'Highway':[highway],'Direction': [direction]})
+        else:
+            new_df = pd.concat([new_df, pd.DataFrame({'Date': [date], 'Time':[time],'Class':[ai], 'RoadSection': [location], 'Highway':[highway], 'Direction': [direction]})], ignore_index=True)
+    
 
 # 將新的 DataFrame 寫入 CSV 檔案，指定編碼格式為 UTF-8-BOM
 new_df.to_csv('accident3_info.csv', index=False, encoding='utf-8-sig')
